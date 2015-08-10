b1ccf52e1f6d7416c658291c74b1624fee37376b
Christophe Marchal <toff63@gmail.com>
2015-08-10 09:33:56 -0300

    Update tom8.bat to match my local configuration

--- karyon-poc/eureka-server-configs/tom8.bat
+++ karyon-poc/eureka-server-configs/tom8.bat
@@ -1,3 +1,3 @@
 CALL j8
-SET CATALINA_HOME=D:\diego\bin\apache-tomcat-8.0.21
-CALL D:\diego\bin\apache-tomcat-8.0.21\bin\catalina.bat start
+SET CATALINA_HOME=E:\docs\bin\apache-tomcat-8.0.24
+CALL E:\docs\bin\apache-tomcat-8.0.24\bin\catalina.bat start
